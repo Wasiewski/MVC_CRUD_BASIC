014bce524b0010a55b30402b5879ee33538694eb
Wasiewski <oskar.wasiewski@wp.pl>
2020-04-23 12:27:47 +0200

    add Controller and simple test case

--- model_view_controller.py
+++ model_view_controller.py
@@ -106,3 +106,80 @@ class View(object):
         print('--------------------------------------------------------------')
         print('We have just removed {} from our list'.format(name))
         print('--------------------------------------------------------------')
+
+
+class Controller(object):
+
+    def __init__(self, model, view):
+        self.model = model
+        self.view = view
+
+    def show_items(self, bullet_points=False):
+        items = self.model.read_items()
+        item_type = self.model.item_type
+        if bullet_points:
+            self.view.show_bullet_point_list(item_type, items)
+        else:
+            self.view.show_number_point_list(item_type, items)
+
+    def show_item(self, item_name):
+        try:
+            item = self.model.read_item(item_name)
+            item_type = self.model.item_type
+            self.view.show_item(item_type, item_name, item)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_missing_item_error(item_name, e)
+
+    def insert_item(self, name, price, quantity):
+        assert price > 0, 'price must be greater than 0'
+        assert quantity >= 0, 'quantity must be greater than or equal to 0'
+        item_type = self.model.item_type
+        try:
+            self.model.create_item(name, price, quantity)
+            self.view.display_item_stored(name, item_type)
+        except mvc_exc.ItemAlreadyStored as e:
+            self.view.display_item_already_stored_error(name, item_type, e)
+
+    def update_item(self, name, price, quantity):
+        assert price > 0, 'price must be greater than 0'
+        assert quantity >= 0, 'quantity must be greater than or equal to 0'
+        item_type = self.model.item_type
+
+        try:
+            older = self.model.read_item(name)
+            self.model.update_item(name, price, quantity)
+            self.view.display_item_updated(
+                name, older['price'], older['quantity'], price, quantity)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_item_not_yet_stored_error(name, item_type, e)
+
+    def update_item_type(self, new_item_type):
+        old_item_type = self.model.item_type
+        self.model.item_type = new_item_type
+        self.view.display_change_item_type(old_item_type, new_item_type)
+
+    def delete_item(self, name):
+        item_type = self.model.item_type
+        try:
+            self.model.delete_item(name)
+            self.view.display_item_deletion(name)
+        except mvc_exc.ItemNotStored as e:
+            self.view.display_item_not_yet_stored_error(name, item_type, e)
+
+
+# tests
+my_items = [
+    {'name': 'bread', 'price': 0.5, 'quantity': 20},
+    {'name': 'milk', 'price': 1.0, 'quantity': 10},
+    {'name': 'wine', 'price': 10.0, 'quantity': 5},
+]
+
+c = Controller(ModelBasic(my_items), View())
+# c.show_items()
+c.show_items(bullet_points=True)
+c.show_item('chocolate')
+c.show_item('bread')
+# we cannot insert the same product twice
+c.insert_item('bread', price=1.0, quantity=5)
+# but we can update item property
+c.update_item('bread', price=1.5, quantity=25)
